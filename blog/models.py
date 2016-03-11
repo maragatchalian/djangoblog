490b620b74a4738723a18c6fe1ea7f9326ef6091
Mara Gatchalian <maragatchalian@KLabs-MacBook-Pro-6.local>
2016-03-11 10:46:51 +0800

    Created a model and an object called 'Post' along with its methods

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Create your models here:
+class Post(models.Moddel): # Model/Object
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(
+		blank=True, null=True)
+
+	def publish(self): #method/function
+		self.published_date = timezone.now()
+		self.save
+
+	def __str__(self):
+		return self.title
