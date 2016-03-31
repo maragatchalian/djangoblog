04002f005804aa092cbce0e374161efc8b8ee0dc
Mara Gatchalian <maragatchalian@KLabs-MacBook-Pro-6.local>
2016-03-31 11:49:28 +0800

    Delete function + or None

--- blog/views.py
+++ blog/views.py
@@ -12,16 +12,23 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
+    form = PostForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.published_date = timezone.now()
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    # if request.method == "POST":
+    #     form = PostForm(request.POST)
+    #     if form.is_valid():
+    #         post = form.save(commit=False)
+    #         post.author = request.user
+    #         post.published_date = timezone.now()
+    #         post.save()
+    #         return redirect('post_detail', pk=post.pk)
+    # else:
+    #     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
@@ -37,3 +44,11 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect('/')
+    else:    
+        return render(request, 'blog/post_delete.html', {'post': post})
