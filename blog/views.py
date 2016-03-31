728d33650fc92edfdeaea5e9e0f3b16d42fda51e
Mara Gatchalian <maragatchalian@KLabs-MacBook-Pro-6.local>
2016-03-31 12:33:53 +0800

    Added cancel button in delete post

--- blog/views.py
+++ blog/views.py
@@ -19,16 +19,6 @@ def post_new(request):
         post.published_date = timezone.now()
         post.save()
         return redirect('post_detail', pk=post.pk)
-    # if request.method == "POST":
-    #     form = PostForm(request.POST)
-    #     if form.is_valid():
-    #         post = form.save(commit=False)
-    #         post.author = request.user
-    #         post.published_date = timezone.now()
-    #         post.save()
-    #         return redirect('post_detail', pk=post.pk)
-    # else:
-    #     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
