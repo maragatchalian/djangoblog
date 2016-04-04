c38c0609715d9e16fcddd700f7a4849504b42723
Mara Gatchalian <maragatchalian@KLabs-MacBook-Pro-6.local>
2016-04-04 09:36:50 +0800

    Date in descending order

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,10 @@ from .models import Post
 from .forms import PostForm
 
 def login(request):
-    return render(request, 'blog/login.html', {})
+    return render(request, 'blog/login.html', {})   
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
