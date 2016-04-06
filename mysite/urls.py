b89e706583d5edf2703d3dea2a21dd32740cac2a
Mara Gatchalian <maragatchalian@KLabs-MacBook-Pro-6.local>
2016-04-06 15:37:34 +0800

    Added redirect url

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
+LOGIN_REDIRECT_URL = '/'
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
