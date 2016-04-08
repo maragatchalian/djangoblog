4e8dc998dfcfbbfcdc5689f6adfbd5d5dddd2a09
Mara Gatchalian <maragatchalian@KLabs-MacBook-Pro-6.local>
2016-04-08 12:28:38 +0800

    Reinitialize

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+# Use this to add, edit, and delete posts
+
+from django.contrib import admin
+from .models import Post, Comment
+
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
